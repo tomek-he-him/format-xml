acd21f3f64a7afbcf0e5ea858bce937404f1cf1e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-02 07:05:10 +0200

    Switch to airbnb code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,45 @@
-/*jshint esnext: false, globalstrict: true */
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-
-
-// Common settings
-// ---------------
-
-var common =
-  { scriptsSource: "source/**/*.js"
-  };
-
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var del = require('del');
 
 // `gulp build`
-// ------------
+// -----------------------------------------------------------------------------
 
-gulp.task("build", ["scripts"]);
-
+gulp.task('build', ['clean', 'scripts']);
 
 // `gulp scripts`
-// --------------
+// -----------------------------------------------------------------------------
 
-gulp.task("scripts", ["scripts:es6", "scripts:es5"]);
+var scripts = {
+  source: 'source/**/*.js'
+};
 
-gulp.task("scripts:es6", function () {
-  return gulp.src(common.scriptsSource)
-    .pipe(gulp.dest("dist.es6"))
-    ;
+gulp.task('scripts', ['scripts:es6', 'scripts:es5']);
+
+gulp.task('scripts:es6',
+  ['scripts:clean'],
+  function() {
+    return gulp.src(scripts.source)
+      .pipe(gulp.dest('dist.es6'))
+      ;
   });
 
-gulp.task("scripts:es5", function () {
-  return gulp.src(common.scriptsSource)
-    .pipe(babel())
-    .pipe(gulp.dest("dist.es5"))
-    ;
+gulp.task('scripts:es5',
+  ['scripts:clean'],
+  function() {
+    return gulp.src(scripts.source)
+      .pipe(babel())
+      .pipe(gulp.dest('dist.es5'))
+      ;
   });
+
+gulp.task('scripts:clean', function(done) {
+  del(scripts.source, done);
+});
+
+// `gulp clean`
+// -----------------------------------------------------------------------------
+
+gulp.task('clean', ['scripts:clean']);
